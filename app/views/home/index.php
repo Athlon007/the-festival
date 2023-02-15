cf4251b2d69676182b25da52f0a6a5975e9565e5
robytrierweiler <roby.trierweiler@energie-pass.lu>
2023-02-15 09:10:19 +0100

    homepage first steps

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -6,11 +6,73 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <meta name=”robots” content="index, follow">
+    <link rel="stylesheet" href="/stylesheet.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Example</title>
 </head>
 
 <body>
-    <p>Hello, world!</p>
+<div class="card mb-3" style="max-width:95%;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Festival</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>  
+    </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3" style="max-width:95%;">
+  <div class="row g-0">
+    <div class="col-md-8">
+    <div class="card-body">
+        <h5 class="card-title">Dance</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>  
+    </div>
+    </div>
+    <div class="col-md-4">
+    <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3" style="max-width:95%;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Yummy!</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>  
+    </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3" style="max-width:95%;">
+  <div class="row g-0">
+    <div class="col-md-8">
+    <div class="card-body">
+        <h5 class="card-title">History</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>  
+    </div>
+    </div>
+    <div class="col-md-4">
+    <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+  </div>
+</div>
+
+
 </body>
 
 </html>
