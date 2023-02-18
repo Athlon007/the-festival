3859fee0f102e36d14652be45f9c8fa4f0bbe7cc
tadevurk <683343@student.inholland.nl>
2023-02-18 10:06:17 +0100

    auth controller, repository

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -2,16 +2,15 @@
 class AuthController
 {
     public function resetPassword(): void
-    {
-        //checking the form has been submitted
-        if (isset($_POST['email'])){
+    {        //checking the form has been submitted
+        if (isset($_POST['submitEmail'])){
             $reset_token = bin2hex(random_bytes(16));
-
-
             $email = $_POST['email'];
-            $time_stamp = time();
-            // here insert the email, reset token, and timestamp into the database (timestamp will be 24 hours from now)
-
+            $sendTime = time();
+            // here insert the email, reset token, and timestamp into the database (timestamp will be 24 hours from now
+            require("../repositories/AuthRepository.php");
+            $authRepository = new AuthRepository();
+            $authRepository->storeResetToken($email, $reset_token, $sendTime);
 
             //send the reset token to the user's email
             $to = $email;
@@ -20,7 +19,7 @@ class AuthController
             . "https://example.com/reset-password.php?token=$reset_token\n\n"
             . "This link will only be valid for 24 hours.";
 
-            $headers = "From: {email address of the sender}\r\n";
+            //$headers = "From: {email address of the sender}\r\n";
             mail($to, $subject, $message, $headers);
 
             echo "A password reset link has been sent to your email address.";
