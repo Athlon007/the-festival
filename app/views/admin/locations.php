39a74c59fdca3d514672b21f2ac78cd95efae060
Athlon007 <aathlon@outlook.com>
2023-03-18 17:28:58 +0100

    Fix Locations editor not working on Webkit/Chromium

--- app/views/admin/locations.php
+++ app/views/admin/locations.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <title>Visit Haarlem Admin - Locations</title>
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="/css/editor.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css">
-    <script src=" https://cdn.tiny.cloud/1/k1ij9ynmcbqgglyj7dnenvnadfy29ad47qa4kewvc2udoshh/tinymce/6/tinymce.min.js" referrerpolicy="origin">
     </script>
 </head>
 
@@ -103,7 +102,7 @@
     <script type="application/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script type="application/javascript" src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
-    <script type=" module" src="/js/admin/locations.js"></script>
+    <script type="module" src="/js/admin/locations.js"></script>
 </body>
 
 </html>
