645e941fed0b1bf7fbf51be800aad67b7a901aa2
Athlon007 <aathlon@outlook.com>
2023-03-21 11:35:03 +0100

    Pusss

--- app/controllers/APIControllers/EventAPIController.php
+++ app/controllers/APIControllers/EventAPIController.php
@@ -60,6 +60,33 @@ class EventAPIController extends APIController
                 }
 
                 echo json_encode($this->service->getJazzEvents($sort, $filters));
+            } elseif (str_starts_with($uri, '/api/events/stroll')) {
+                require_once(__DIR__ . "/../services/FestivalHistoryService.php");
+                $strollService = new FestivalHistoryService();
+
+                if (is_numeric(basename($uri))) {
+                    $id = basename($uri);
+                    echo json_encode($event);
+                    return;
+                }
+
+                if (isset($_GET['date'])) {
+                    $filters['date'] = $_GET['date'];
+                }
+
+                if (isset($_GET['time'])) {
+                    $filters['time'] = $_GET['time'];
+                }
+
+                if (isset($_GET['language'])) {
+                    $filters['language'] = $_GET['language'];
+                }
+
+                if (isset($_GET['ticket_type'])) {
+                    $filters['ticket_type'] = $_GET['ticket_type'];
+                }
+
+                echo json_encode($strollService->getAllHistoryEvents()));
             } else {
                 if (is_numeric(basename($uri))) {
                     $id = basename($uri);
--- app/public/js/modules/events.js
+++ app/public/js/modules/events.js
@@ -47,171 +47,50 @@ class EventsList {
     }
 
     async init(container) {
-        // create the sorting columns
-        this.sortingContainer = document.createElement('div');
-        this.sortingContainer.classList.add('col-3', 'p-1');
-        container.appendChild(this.sortingContainer);
+        let eventContainer = document.createElement('div');
+        eventContainer.classList.add('container', 'ticket-container');
 
-        let eventsMain = document.createElement('div');
-        eventsMain.classList.add('col-8');
+        let rowContent = document.createElement('div');
+        rowContent.classList.add('row');
+        eventContainer.appendChild(rowContent);
 
-        this.eventsContainer = document.createElement('div');
-        this.eventsContainer.classList.add('container', 'gy-1', 'overflow-auto');
-        this.eventsContainer.style.maxHeight = '500px';
-        eventsMain.appendChild(this.eventsContainer);
+        let colSorts = document.createElement('div');
+        colSorts.classList.add('col-md-6');
+        rowContent.appendChild(colSorts);
 
-        container.appendChild(eventsMain);
-    }
+        let rowDateTime = document.createElement('div');
+        rowDateTime.classList.add('row', 'mb-3');
+        colSorts.appendChild(rowDateTime);
 
-    addEventToCard(event, amount) {
-    }
-}
+        let colDate = document.createElement('div');
+        colDate.classList.add('col-md-6', 'dateOfTour');
+        colDate.style.width = '50%';
+        colDate.style.float = 'left';
+        rowDateTime.appendChild(colDate);
 
-class JazzEventList extends EventsList {
-    constructor(container) {
-        super(container);
-        this.loadEvents();
-    }
+        let labelColDate = document.createElement('label');
+        labelColDate.innerText = 'Date';
+        labelColDate.classList.add('form-label');
 
-    async init(container) {
-        super.init(container);
-
-        // SORT
-        let sortRow = document.createElement('div');
-        sortRow.classList.add('row');
-        let sortHeader = document.createElement('h2');
-        sortHeader.innerText = 'Sort';
-        let sorts = document.createElement('select');
-        sorts.classList.add('form-select');
-        sorts.addEventListener('change', (e) => {
-            this.sortMethod = e.target.value;
+        let selectColDate = document.createElement('select');
+        selectColDate.classList.add('form-select');
+        selectColDate.id = 'dateOfTour';
+        selectColDate.addEventListener('change', (e) => {
+            this.dateOfTour = e.target.value;
             this.loadEvents();
         });
 
-        let sortOptions = [
-            { value: 'time-asc', text: 'Time ascending' },
-            { value: 'time-desc', text: 'Time descending' },
-            { value: 'price-asc', text: 'Price ascending' },
-            { value: 'price-desc', text: 'Price descending' }
-        ];
-        for (let option of sortOptions) {
-            let sortOption = document.createElement('option');
-            sortOption.value = option.value;
-            sortOption.innerText = option.text;
-            sorts.appendChild(sortOption);
-        }
+        let optionColDate = document.createElement('option');
+        optionColDate.innerText = 'Choose Date';
+        optionColDate.value = '-1';
+        selectColDate.appendChild(optionColDate);
 
-        sortRow.appendChild(sortHeader);
-        sortRow.appendChild(sorts);
-        this.sortingContainer.appendChild(sortRow);
 
-        // FILTER
-        // Filter Time
-        let timeFilter = document.createElement('div');
-        timeFilter.classList.add('row');
-        let timeHeader = document.createElement('h2');
-        timeHeader.innerText = 'Time';
-        //start
-        let timeStartHeader = document.createElement('h3');
-        timeStartHeader.innerText = 'From';
-        let timeStart = document.createElement('select');
-        timeStart.classList.add('form-select');
-        timeStart.addEventListener('change', (e) => {
-            this.timeStart = e.target.value;
-            this.loadEvents();
-        });
-        //end
-        let timeEndHeader = document.createElement('h3');
-        timeEndHeader.innerText = 'To';
-        let timeEnd = document.createElement('select');
-        timeEnd.classList.add('form-select');
-        timeEnd.addEventListener('change', (e) => {
-            this.timeEnd = e.target.value;
-            this.loadEvents();
-        });
-
-        // add hours from 0 to 23
-        for (let i = 0; i < 24; i++) {
-            let optionStart = document.createElement('option');
-            let optionEnd = document.createElement('option');
-            optionStart.value = optionEnd.value = i;
-            optionStart.innerText = optionEnd.innerText = i + ':00';
-            timeStart.appendChild(optionStart);
-            timeEnd.appendChild(optionEnd);
-        }
-
-        timeFilter.appendChild(timeHeader);
-        timeFilter.appendChild(timeStartHeader);
-        timeFilter.appendChild(timeStart);
-        timeFilter.appendChild(timeEndHeader);
-        timeFilter.appendChild(timeEnd);
-        this.sortingContainer.appendChild(timeFilter);
-
-        // Price
-        let priceFilter = document.createElement('div');
-        priceFilter.classList.add('row');
-        let priceHeader = document.createElement('h2');
-        priceHeader.innerText = 'Price';
-        let priceFromHeader = document.createElement('h3');
-        priceFromHeader.innerText = 'From';
-        let priceFrom = document.createElement('input');
-        priceFrom.type = 'number';
-        priceFrom.classList.add('form-control');
-        priceFrom.addEventListener('change', (e) => {
-            this.priceFrom = e.target.value;
-            this.loadEvents();
-        }
-        );
-
-        let priceToHeader = document.createElement('h3');
-        priceToHeader.innerText = 'To';
-        let priceTo = document.createElement('input');
-        priceTo.type = 'number';
-        priceTo.classList.add('form-control');
-        priceTo.addEventListener('change', (e) => {
-            this.priceTo = e.target.value;
-            this.loadEvents();
-        }
-        );
-
-        priceFilter.appendChild(priceHeader);
-        priceFilter.appendChild(priceFromHeader);
-        priceFilter.appendChild(priceFrom);
-        priceFilter.appendChild(priceToHeader);
-        priceFilter.appendChild(priceTo);
-        this.sortingContainer.appendChild(priceFilter);
-
-        // Attributes
-        let attributesFilter = document.createElement('div');
-        attributesFilter.classList.add('row');
-        let attributesHeader = document.createElement('h2');
-        attributesHeader.innerText = 'Attributes';
-        // hide events without seats checkbox
-        let hideWithoutSeatsDiv = document.createElement('div');
-        hideWithoutSeatsDiv.classList.add('form-check');
-        let hideWithoutSeats = document.createElement('input');
-        hideWithoutSeats.type = 'checkbox';
-        hideWithoutSeats.classList.add('form-check-input');
-        hideWithoutSeats.addEventListener('change', (e) => {
-            this.hideWithoutSeats = e.target.checked;
-            this.loadEvents();
-        }
-        );
-        let hideWithoutSeatsLabel = document.createElement('label');
-        hideWithoutSeatsLabel.classList.add('form-check-label');
-        hideWithoutSeatsLabel.innerText = 'Hide events without seats';
-        hideWithoutSeatsLabel.htmlFor = 'hideWithoutSeats';
-
-        attributesFilter.appendChild(attributesHeader);
-        hideWithoutSeatsDiv.appendChild(hideWithoutSeats);
-        hideWithoutSeatsDiv.appendChild(hideWithoutSeatsLabel);
-        attributesFilter.appendChild(hideWithoutSeatsDiv);
-        this.sortingContainer.appendChild(attributesFilter);
     }
 
     async getData() {
         // if sort by is set
-        let url = '/api/events/jazz';
+        let url = '/api/events/stroll';
         let args = '';
 
         function addArg(arg) {
@@ -263,72 +142,6 @@ class JazzEventList extends EventsList {
 
     addEvent(event) {
         super.addEvent(event);
-
-        let eventContainer = document.createElement('div');
-        eventContainer.classList.add('row', 'card');
-        eventContainer.id = 'event-' + event.id;
-
-        let rowTitle = document.createElement('div');
-        rowTitle.classList.add('row');
-        let title = document.createElement('h2');
-        title.innerText = event.name;
-        rowTitle.appendChild(title);
-
-        let rowDetails = document.createElement('div');
-        rowDetails.classList.add('row');
-
-        rowDetails.appendChild(this.createDetailBox('Location', event.location.name));
-
-        const startTime = new Date(event.startTime.date);
-        const endTime = new Date(event.endTime.date);
-        const startHour = startTime.getHours();
-        const endHour = endTime.getHours();
-        const startMinutes = startTime.getMinutes();
-        const endMinutes = endTime.getMinutes();
-        const startMinutesString = startMinutes < 10 ? '0' + startMinutes : startMinutes;
-        const endMinutesString = endMinutes < 10 ? '0' + endMinutes : endMinutes;
-        const displayTime = `${startTime.toDateString()}<br> ${startHour}:${startMinutesString} - ${endHour}:${endMinutesString}`;
-        rowDetails.appendChild(this.createDetailBox('Time', displayTime));
-
-        rowDetails.appendChild(this.createDetailBox('Seats', event.location.capacity));
-        rowDetails.appendChild(this.createDetailBox('Price', event.price));
-
-        // buttons row
-        let rowButtons = document.createElement('div');
-        rowButtons.classList.add('row', 'justify-content-end', 'py-2', 'gx-2', 'px-0');
-        // amount input
-        let amountInput = document.createElement('input');
-        amountInput.type = 'number';
-        amountInput.classList.add('form-control');
-        amountInput.value = 1;
-        amountInput.min = 1;
-        amountInput.max = 10;
-        amountInput.style.width = '4.5em';
-        amountInput.style.marginRight = '0.5em';
-        // buy button
-        let buyButton = document.createElement('button');
-        buyButton.classList.add('btn', 'btn-primary', 'col-3');
-        buyButton.innerText = 'Add ticket to cart';
-        buyButton.addEventListener('click', () => {
-            let amount = amountInput.value;
-            this.addEventToCard(event.id, amount);
-        });
-        let buttonDetailsA = document.createElement('a');
-        buttonDetailsA.href = `/festival/jazz/event/${event.id}`;
-        buttonDetailsA.classList.add('col-3');
-        let buttonDetails = document.createElement('button');
-        buttonDetails.classList.add('btn', 'btn-secondary', 'w-100');
-        buttonDetails.innerText = 'About event';
-        buttonDetailsA.appendChild(buttonDetails);
-        rowButtons.appendChild(amountInput);
-        rowButtons.appendChild(buyButton);
-        rowButtons.appendChild(buttonDetailsA);
-
-        eventContainer.appendChild(rowTitle);
-        eventContainer.appendChild(rowDetails);
-        eventContainer.appendChild(rowButtons);
-
-        this.eventsContainer.appendChild(eventContainer);
     }
 
     createDetailBox(name, value) {
