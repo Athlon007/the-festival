9b9a512f0e3276fb3ed21b604bf0835b92d769f0
Athlon007 <aathlon@outlook.com>
2023-04-06 15:44:09 +0200

    Add universal admin navbar

---
+++ app/views/admin/adminNavbar.php
@@ -0,0 +1,22 @@
+<nav class="nav nav-pills flex-column flex-sm-row" id="admin-nav">
+    <a class="flex-sm-fill text-sm-center nav-link" href="/manageTextPages">Text Pages</a>
+    <a class="flex-sm-fill text-sm-center nav-link" href="/manageImages">Images</a>
+    <a class="flex-sm-fill text-sm-center nav-link" href="/manageUsers">Users</a>
+
+    <a class="flex-sm-fill text-sm-center nav-link" href="/manageJazz">Jazz</a>
+    <a class="flex-sm-fill text-sm-center nav-link" href="/manageRestaurants">Restaurants</a>
+    <a class="flex-sm-fill text-sm-center nav-link" href="/manageDJs">Dance</a>
+    <a class="flex-sm-fill text-sm-center nav-link" href="/manageHistory">History</a>
+    <a class="flex-sm-fill text-sm-center nav-link" href="/manageTicketTypes">Ticket Types</a>
+    <a class="flex-sm-fill text-sm-center nav-link" href="/managePasses">Passes</a>
+    <a class="flex-sm-fill text-sm-center nav-link" href="/manageNavBar">Nav Bar</a>
+</nav>
+
+<script>
+    // Mark the a active, which has the same href as the current page.
+    for (const a of document.getElementById('admin-nav').querySelectorAll('a')) {
+        if (window.location.href.includes(a.href)) {
+            a.classList.add('active');
+        }
+    }
+</script>
