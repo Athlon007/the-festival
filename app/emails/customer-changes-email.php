c47ca3d8e6317baeabc5a937fd4f1009a8bcd366
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2023-04-14 14:39:11 +0200

    mail

---
+++ app/emails/customer-changes-email.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Changes to Account</title>
+    </head>
+    <body>
+        <h2>
+            Account changes
+        </h2>
+        <p>
+            Dear customer,
+        </p>
+        <p>
+            Changes have been made to your account. If you did not make these changes, contact us immediately by replying to this e-mail.
+        </p>
+        <p>
+            Your account number: <?php echo $customer->getUserId(); ?>
+        </p>
+    </body>
+</html>
