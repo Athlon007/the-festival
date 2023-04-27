96cce30a37ca6ada237071b6b9d1ed0276dceaa5
Vedat Türk <683343@student.inholland.nl>
2023-04-27 12:21:32 +0200

    festival controller

--- app/controllers/FestivalHistoryController.php
+++ app/controllers/FestivalHistoryController.php
@@ -4,6 +4,12 @@ require_once("../services/TicketLinkService.php");
 
 class FestivalHistoryController
 {
+    private $festivalHistoryService;
+
+    public function __construct()
+    {
+        $this->festivalHistoryService = new FestivalHistoryService();
+    }
     public function loadHistoryStrollPage()
     {
         try {
@@ -11,6 +17,16 @@ class FestivalHistoryController
             $historyTicketLinks = $cartItemService->getAll();
 
             require("../views/festival/history-stroll.php");
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function getAllHistoryEvents(){
+        try{
+            $historyEvents = $this->festivalHistoryService->getAllHistoryEvents();
+
+            require("../views/admin/History Management/manageHistory.php");
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
--- app/services/FestivalHistoryService.php
+++ app/services/FestivalHistoryService.php
@@ -1,31 +1,18 @@
 <?php
+require_once(__DIR__ . "/../repositories/FestivalHistoryRepository.php");
 
 class FestivalHistoryService
 {
+    private $festivalHistoryRep;
+
+    public function __construct()
+    {
+        $this->festivalHistoryRep = new FestivalHistoryRepository();
+    }
+
     public function getAllHistoryEvents()
     {
-        try{
-            require_once("../repositories/FestivalHistoryRepository.php");
-            $festivalHistoryRepository = new FestivalHistoryRepository();
-            $historyEvents = $festivalHistoryRepository->getAllHistoryEvents();
-
-            return $historyEvents;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
+        return $this->festivalHistoryRep->getAllHistoryEvents();
     }
 
-    public function getAllTourLocations()
-    {
-        try{
-            // Call location repository
-            require_once("../repositories/LocationRepository.php");
-            $locationRepository = new LocationRepository();
-            $locations = $locationRepository->getAllHistoryLocations();
-
-            return $locations;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
 }
