93ba9dafaa4a899cc4ca40d0e5037689c52f39e3
Vedat Türk <683343@student.inholland.nl>
2023-05-04 11:47:33 +0200

    small change

--- app/views/admin/exportOrders.php
+++ app/views/admin/exportOrders.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Admin Orders</title>
+    <title>Orders</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
@@ -20,7 +20,7 @@
 <body>
 
     <div class="container">
-        <h1 class="my-5">Admin Orders</h1>
+        <h1 class="my-5">Orders</h1>
         <table class="table table-bordered">
             <thead>
                 <tr>
