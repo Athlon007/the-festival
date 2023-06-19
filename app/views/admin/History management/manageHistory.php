fc9765017323c7265898edfd08f05f335f953738
Vedat Türk <683343@student.inholland.nl>
2023-06-19 11:55:11 +0200

    delete history event

--- app/views/admin/History management/manageHistory.php
+++ app/views/admin/History management/manageHistory.php
@@ -9,7 +9,8 @@
     <meta name="theme-color" content="#fffbfa">
     <meta name="robots" content="noindex, nofollow">
     <title>Visit Haarlem - Manage Users</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/icons.css">
 </head>
@@ -69,7 +70,14 @@
                             </td>
                             <!-- Update button -->
                             <td>
-                                <a href="/updateHistoryTour?id=<?php echo $historyEvent->getId(); ?>" class="btn btn-primary">Update</a>
+                                <a href="/updateHistoryTour?id=<?php echo $historyEvent->getId(); ?>"
+                                    class="btn btn-primary">Update</a>
+                                <!-- Delete button -->
+                            <td>
+                            <td>
+                                <button class="btn btn-danger delete-btn"
+                                    data-event-id="<?php echo $historyEvent->getId(); ?>">Delete</button>
+                            </td>
                         </tr>
                     <?php } ?>
                 </tbody>
@@ -85,25 +93,68 @@
     </div>
     </div>
     <div id="Location" class="data">
-        <iframe id="iframe" src="/admin/locations" data-locations="3" style="width: 100%; height: 900px; border: none; margin-left:1em; margin-right:1em;"></iframe>
+        <iframe id="iframe" src="/admin/locations" data-locations="3"
+            style="width: 100%; height: 900px; border: none; margin-left:1em; margin-right:1em;"></iframe>
     </div>
 
     </div>
     <br>
     <footer class="foot row bottom"></footer>
-    <script type="application/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script type="application/javascript"
+        src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
 
     <script type="module" src="/js/foot.js"></script>
     <script>
-        $(document).ready(function() {
-            $("#name").on("change", function() {
-                    //alert($(this).val());
-                    $(".data").hide();
-                    $("#" + $(this).val()).fadeIn(700);
-                })
+        $(document).ready(function () {
+            $("#name").on("change", function () {
+                //alert($(this).val());
+                $(".data").hide();
+                $("#" + $(this).val()).fadeIn(700);
+            })
                 .change();
         });
+
+
+        // Add event listeners to delete buttons
+        const deleteButtons = document.getElementsByClassName("delete-btn");
+        Array.from(deleteButtons).forEach((button) => {
+            button.addEventListener("click", () => {
+                const eventId = button.getAttribute("data-event-id");
+                const confirmDelete = confirm("Are you sure you want to delete this event?");
+                if (confirmDelete) {
+                    deleteEvent(eventId);
+                }
+            });
+        });
+
+        const deleteEvent = async (id) => {
+            try {
+                const response = await fetch(`/api/events/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                });
+
+                const data = await response.json();
+
+                if (response.ok) {
+                    // Event deleted successfully, display success message and reload the page
+                    alert(data.success_message);
+                    location.reload();
+                } else {
+                    // An error occurred while deleting the event, display error message
+                    alert(data.error_message);
+                }
+            } catch (error) {
+                console.error('Error deleting event:', error);
+                // Display a generic error message
+                alert('An error occurred while deleting the event.');
+            }
+        };
     </script>
 
 </body>
